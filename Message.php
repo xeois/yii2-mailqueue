13b68fe1141b446f233067012419cec1552b9ea4
hussain <m.hussain@xeois.com>
2017-02-04 13:47:57 +0530

    Updated to align with composer

--- Message.php
+++ Message.php
@@ -2,10 +2,10 @@
 
 /**
  * Message.php
- * @author Mohammad Hussain http://xeois.com
+ * @author Mohammad Hussain http://mhussa.in
  */
 
-namespace xeois\mailqueue;
+namespace mhussain001\mailqueue;
 
 use Yii;
 use nterms\mailqueue\models\Queue;
@@ -15,7 +15,7 @@ use nterms\mailqueue\models\Queue;
  *
  * @see http://www.yiiframework.com/doc-2.0/yii-swiftmailer-message.html
  */
-class Message extends \yii\swiftmailer\Message
+class Message extends nterms\mailqueue\Message
 {
     /**
      * Enqueue the message storing it in database.
@@ -23,8 +23,28 @@ class Message extends \yii\swiftmailer\Message
      * @param timestamp $time_to_send
      * @return boolean true on success, false otherwise
      */
-    public function queue($time_to_send = 'now')
+    public function queue($time_to_send = strtotime("+5 minutes"))
     {
+        return $this->mailProcess($time_to_send);
+    }
+
+    /**
+     * Send mail immediately and store in database for audit 
+     * @return boolean true on success, false otherwise
+     */
+    
+    public functin sendNow()
+    {
+        return $this->mailProcess()
+    }
+
+    /**
+     * @param timestamp $time_to_send
+     * Process mail based on time_to_send. If time to send is now then it will send mail right away else it will be queued
+     * @return boolean true on success, false otherwise
+     */
+    
+    protected function mailProcess($time_to_send = 'now')
         if($time_to_send == 'now') {
             $time_to_send = time();
         }
@@ -63,6 +83,22 @@ class Message extends \yii\swiftmailer\Message
                 if( !$item->charset ) {
                     $item->charset = $part->getCharset();
                 }
+            }
+        }
+        /**
+         if time to send is now then send mail and track in db
+        */
+        if($time_to_send == 'now') {
+            if ($message = $item->toMessage()) {
+                if ($this->send($message)) {
+                    $item->sent_time = new \yii\db\Expression('NOW()');
+                    $attributes[] = 'sent_time';
+                } else {
+                    $success = false;
+                }
+
+                $item->attempts++;
+                $item->last_attempt_time = new \yii\db\Expression('NOW()');
             }
         }
 
--- README.md
+++ README.md
@@ -4,5 +4,5 @@ extension of nterms/yii2-mailque to send and queue. Email queue component for yi
 This is enhance version of https://github.com/nterms/yii2-mailqueue.
 
 Following functionality has been update
-1) Queue message for send after default 5 mins
-2) SendNow feature send mail immediately without adding to queue, while add and entry to DB for further auditing
+* Queue message for send after default 5 mins
+* SendNow feature send mail immediately without adding to queue, while add and entry to DB for further auditing
